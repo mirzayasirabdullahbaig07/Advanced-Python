04e7cd248af205761c5fd62a9b98891490e411a6
mirzayasirabdullahbaig07 <yasirabdullah4549@gmail.com>
2025-08-03 17:47:25 +0500

    While Loop Questions Added

--- 08_Practice_Of_Python/Practice_Advanced_Python.py
+++ 08_Practice_Of_Python/Practice_Advanced_Python.py
@@ -490,3 +490,71 @@ else:
         else:
             print("You are obese.")
 
+# BASIC WHILE LOOP
+# Q42. Ask a number from user. Print all the numbers from 1 to that number.
+user_do = int(input("enter the number = "))
+i = 1
+while i <= user_do:
+    print(i)
+    i  = i + 1
+    
+# Q43. Ask a number (N) from user. Print all the numbers from N to 1.
+user_do = int(input("enter the number = "))
+i = user_do
+while i >= 1:
+    print(i)
+    i = i - 1
+# Q44. Ask start number and end number from user. Print all the numbers
+# from start to end using while loop.
+start = int(input("enter the first number "))
+end = int(input("enter the last number "))
+i = start
+while i <= end:
+    print(i)
+    i = i + 1
+# Q45. Calculate the sum of all the numbers from 1 to 10.
+i = 1
+total = 0
+
+while i <= 10:
+    total = total + i
+    i = i + 1
+print(f"sum is {total}")
+# Q46. Calculate product of all the numbers from 1 to 10.
+
+i = 1
+total = 1
+
+while i <= 10:
+    total = total * i
+    i = i + 1
+print(f"the total product is {total}")
+# Q47. Calculate how many numbers are divisible by 7 from 1 to 100.
+i = 1
+count = 0
+while i <= 100:
+    if i % 7 == 0:
+       count = count + 1
+    i = i + 1
+print(f"the value is {count}")
+# Q48. Calculate how many numbers are divisible by both 6 and 7 between 1
+# to 200.
+i = 1
+count = 0
+
+while i <= 200:
+    if i % 6 == 0 and i % 7 == 0:
+        count = count + 1
+    i = i + 1
+print(f"the total count is {count}")
+# Q49. Write a program to calculate the sum of all the numbers divisible by 4
+# from 20 to 50.
+num = 0
+i = 20
+while i <= 50:
+    if i % 4 == 0:
+        num = num + i
+    i = i + 1
+print(f"the sum is {num}")
+
+
