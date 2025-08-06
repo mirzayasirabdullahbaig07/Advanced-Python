b3804f9efb87c4f1472f0b18ea448ea39e4a60e8
mirzayasirabdullahbaig07 <yasirabdullah4549@gmail.com>
2025-08-06 22:04:43 +0500

    file updated

--- 08_Practice_Of_Python/Practice_Advanced_Python.py
+++ 08_Practice_Of_Python/Practice_Advanced_Python.py
@@ -1019,3 +1019,41 @@ for i in range(1, 6):
         
     print()
 
+
+# Q87
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+for i in range(1, 6):
+    for j in range(5 - i):
+        print(" ", end="")
+    for k in range(2 * i - 1):
+        print("*", end="")
+    print()
+
+for i in range(4, 0, -1):
+    for j in range(5 - i):
+        print(" ", end="")
+    for k in range(2 * i - 1):
+        print("*", end="")
+    print()
+
+*********
+ *******
+  *****
+   ***
+    *
+
+
+for i in range(4, 0, -1):
+    for j in range(5 - i):
+        print(" ", end="")
+    for k in range(2 * i - 1):
+        print("*", end="")
+    print()
