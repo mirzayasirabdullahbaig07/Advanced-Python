c00198fc04c883de0875ce6dab9aeef20f29b045
mirzayasirabdullahbaig07 <yasirabdullah4549@gmail.com>
2025-08-14 21:54:58 +0500

    python program added

--- 08_Practice_Of_Python/Practice_Advanced_Python.py
+++ 08_Practice_Of_Python/Practice_Advanced_Python.py
@@ -1533,3 +1533,26 @@ print(result)
 str_ = "yasir            abdullah"
 
 print(str_.count(" "))
+
+# Q137. Ask a string from user. Print the count of how many alphabets, digits, spaces and symbols (everything else) are there in that string. (Do on your own)
+text = input("enter the string: ")
+
+alphabets = 0
+digits = 0
+spaces = 0
+symbols = 0
+
+for char in text:
+    if char.isalpha():
+        alphabets += 1
+    elif char.isdigit():
+        digits += 1
+    elif char.isspace():
+        space += 1
+    else:
+        symbols += 1
+
+print("Alphabets:", alphabets)
+print("Digits:", digits)
+print("Spaces:", spaces)
+print("Symbols:", symbols)
